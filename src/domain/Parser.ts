ce6cbee244ab85083362821708dcebb19592633b
takiuchi <kazuma_takiuchi@scsk-hokkaido.co.jp>
2021-10-26 12:50:09 +0900

    add parserFeeds

--- src/domain/Parser.ts
+++ src/domain/Parser.ts
@@ -2,6 +2,21 @@ import Parser from "rss-parser";
 
 export const parseFeed = async (url: string) => {
   const parser = new Parser();
+
   const feed = await parser.parseURL(url);
   return feed;
+};
+
+export const parseFeeds = async (urls: string[]) => {
+  const promises = urls.map((url) => {
+    return parseFeed(url);
+  });
+
+  const tmpResult = await Promise.all(promises);
+
+  const result = tmpResult.map((elem) => {
+    return elem;
+  });
+
+  return result;
 };
--- test/Parser.test.ts
+++ test/Parser.test.ts
@@ -1,4 +1,4 @@
-import { parseFeed } from "../src/domain/Parser";
+import { parseFeed, parseFeeds } from "../src/domain/Parser";
 // import { parseFeed } from "@src/domain/Parser";
 
 describe("parserFeed", () => {
@@ -16,5 +16,25 @@ describe("parserFeed", () => {
     //   console.log(result);
 
     expect(result.items.length).toBe(2);
+  });
+});
+
+describe("parserFeeds", () => {
+  test("ArrayFeeds", async () => {
+    const feeds = [
+      "https://www.feedforall.com/sample.xml",
+      "https://so-zou.jp/web-app/tech/feed/atom/sample.atom",
+    ];
+    const result = await parseFeeds(feeds);
+
+    const itemsCount = result.map((elem) => {
+      return elem.items.length;
+    });
+
+    const sumCount = itemsCount.reduce((preVal, curVal) => {
+      return preVal + curVal;
+    }, 0);
+
+    expect(sumCount).toBe(11);
   });
 });
