5a1957746b0c0149f021ad6168c7fc57f89a1756
kazuma takiuchi <basstrombone413@gmail.com>
2021-10-26 17:41:10 +0900

    fix parse function

--- src/domain/Parser.ts
+++ src/domain/Parser.ts
@@ -1,22 +1,35 @@
+import dayjs from "dayjs";
 import Parser from "rss-parser";
 
-export const parseFeed = async (url: string) => {
+export const parseFeed = async (arg: { site_name: string; url: string }) => {
   const parser = new Parser();
 
-  const feed = await parser.parseURL(url);
-  return feed;
+  const feed = await parser.parseURL(arg.url);
+  const result = feed.items.map((elem) => {
+    elem.site_name = arg.site_name;
+    return elem;
+  });
+  return result;
 };
 
-export const parseFeeds = async (urls: string[]) => {
+export const parseFeeds = async (
+  urls: { site_name: string; url: string }[]
+) => {
   const promises = urls.map((url) => {
     return parseFeed(url);
   });
 
   const tmpResult = await Promise.all(promises);
 
-  const result = tmpResult.map((elem) => {
-    return elem;
-  });
+  const result = tmpResult
+    .flatMap((elem) => {
+      return elem;
+    })
+    .sort((a, b) => {
+      const dayA = dayjs(a.isoDate);
+      const dayB = dayjs(b.isoDate);
+      return dayB.diff(dayA);
+    });
 
   return result;
 };
--- test/Parser.test.ts
+++ test/Parser.test.ts
@@ -3,38 +3,59 @@ import { parseFeed, parseFeeds } from "../src/domain/Parser";
 
 describe("parserFeed", () => {
   test("Rss Feed ", async () => {
-    const result = await parseFeed("https://www.feedforall.com/sample.xml");
+    const arg = {
+      site_name: "sampleRSS",
+      url: "https://www.feedforall.com/sample.xml",
+    };
+    const result = await parseFeed(arg);
     //   console.log(result);
 
-    expect(result.items.length).toBe(9);
+    expect(result.length).toBe(9);
+    expect(result[0].site_name).toBe("sampleRSS");
   });
 
   test("Atom Feed ", async () => {
-    const result = await parseFeed(
-      "https://so-zou.jp/web-app/tech/feed/atom/sample.atom"
-    );
+    const arg = {
+      site_name: "sampleAtom",
+      url: "https://so-zou.jp/web-app/tech/feed/atom/sample.atom",
+    };
+    const result = await parseFeed(arg);
     //   console.log(result);
 
-    expect(result.items.length).toBe(2);
+    expect(result.length).toBe(2);
+    expect(result[0].site_name).toBe("sampleAtom");
   });
 });
 
 describe("parserFeeds", () => {
   test("ArrayFeeds", async () => {
     const feeds = [
-      "https://www.feedforall.com/sample.xml",
-      "https://so-zou.jp/web-app/tech/feed/atom/sample.atom",
+      {
+        site_name: "sampleRSS",
+        url: "https://www.feedforall.com/sample.xml",
+      },
+      {
+        site_name: "sampleAtom",
+        url: "https://so-zou.jp/web-app/tech/feed/atom/sample.atom",
+      },
     ];
     const result = await parseFeeds(feeds);
 
-    const itemsCount = result.map((elem) => {
-      return elem.items.length;
-    });
+    // 2つのRSSが混在している状態
+    expect(result.length).toBe(11);
+    expect(
+      result.filter((elem) => {
+        return elem.site_name === "sampleRSS";
+      }).length
+    ).toBe(9);
+    expect(
+      result.filter((elem) => {
+        return elem.site_name === "sampleAtom";
+      }).length
+    ).toBe(2);
 
-    const sumCount = itemsCount.reduce((preVal, curVal) => {
-      return preVal + curVal;
-    }, 0);
-
-    expect(sumCount).toBe(11);
+    // 日付の降順
+    expect(result[0].isoDate).toBe("2012-12-31T17:00:00.000Z");
+    expect(result[10].isoDate).toBe("2004-10-19T15:08:56.000Z");
   });
 });
