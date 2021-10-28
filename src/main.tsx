912c9abdfad23535d61395c6f843310cb232e3c5
takiuchi <kazuma_takiuchi@scsk-hokkaido.co.jp>
2021-10-28 15:37:02 +0900

    add pwa setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
+
+serviceWorkerRegistration.register();
