1b520f947b57c3d80894786010ea1edf0b8ff945
takiuchi <kazuma_takiuchi@scsk-hokkaido.co.jp>
2021-10-28 14:35:18 +0900

    fix rendring count reduce

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -14,33 +14,38 @@ import { rssArticlesAtom } from "../recoil/Atoms";
 
 const Timeline = () => {
   const rssArticles = useRecoilValue(rssArticlesAtom);
+  const baseDate = dayjs().subtract(1, "w");
 
-  const timeLineList = rssArticles.map((elem, idx) => {
-    return (
-      <Box pt={1} key={idx}>
-        <Flex>
-          <Avatar
-            size="xs"
-            bgColor="white"
-            name={elem.site_name}
-            loading="lazy"
-            src={`https://www.google.com/s2/favicons?sz=64&domain=${elem.domain}`}
-          />
-          <Text pl={1}>{elem.site_name}</Text>
-          <Spacer />
-          <Text color="gray">
-            {dayjs(elem.updatedParsed).format("YYYY/MM/DD HH:mm:ss")}
-          </Text>
-        </Flex>
-        <Box ml={7}>
-          <Link color="blue.400" href={elem.link} isExternal>
-            {elem.title}
-          </Link>
+  const timeLineList = rssArticles
+    .filter((elem) => {
+      return dayjs(elem.updatedParsed).isAfter(baseDate);
+    })
+    .map((elem, idx) => {
+      return (
+        <Box pt={1} key={idx}>
+          <Flex>
+            <Avatar
+              size="xs"
+              bgColor="white"
+              name={elem.site_name}
+              loading="lazy"
+              src={`https://www.google.com/s2/favicons?sz=64&domain=${elem.domain}`}
+            />
+            <Text pl={1}>{elem.site_name}</Text>
+            <Spacer />
+            <Text color="gray">
+              {dayjs(elem.updatedParsed).format("YYYY/MM/DD HH:mm:ss")}
+            </Text>
+          </Flex>
+          <Box ml={7}>
+            <Link color="blue.400" href={elem.link} isExternal>
+              {elem.title}
+            </Link>
+          </Box>
+          <Divider />
         </Box>
-        <Divider />
-      </Box>
-    );
-  });
+      );
+    });
   return (
     <Box h="calc(100vh - 55px)" sx={{ overflow: "scroll" }}>
       {timeLineList}
@@ -55,15 +60,5 @@ let TimelineList: {
   title: string;
   url: string;
 }[] = [];
-
-for (let index = 0; index < 20; index++) {
-  TimelineList.push({
-    site_name: "Qiita",
-    update_date: "2021/10/23 20:31:22",
-    icon_url: "http://www.google.com/s2/favicons?domain=qiita.com",
-    title: "Mac BigSurでM5Stamp C3(ESP32 C3)にArduino IDEで書き込みたい",
-    url: "https://qiita.com/necobut/items/8c45af1a8ba975697a7d?utm_campaign=popular_items&utm_medium=feed&utm_source=popular_items",
-  });
-}
 
 export default Timeline;
