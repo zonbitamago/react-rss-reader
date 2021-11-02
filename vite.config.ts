bb61a329023c9cb2655b4a1356193cd7219a0f2e
takiuchi <kazuma_takiuchi@scsk-hokkaido.co.jp>
2021-11-02 13:15:45 +0900

    add package vizualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,61 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "apple-touch-icon.png",
-      ],
-      manifest: {
-        name: "Name of your app",
-        short_name: "Short name of your app",
-        description: "Description of your app",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      rollupOptions: {
+        plugins: [
+          mode === "analyze" &&
+            visualizer({
+              open: true,
+              filename: "dist/stats.html",
+              gzipSize: true,
+              brotliSize: true,
+            }),
         ],
       },
-    }),
-  ],
-  // define: {
-  //   global: {},
-  // },
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        includeAssets: [
+          "favicon.svg",
+          "favicon.ico",
+          "robots.txt",
+          "apple-touch-icon.png",
+        ],
+        manifest: {
+          name: "Name of your app",
+          short_name: "Short name of your app",
+          description: "Description of your app",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+          ],
+        },
+      }),
+    ],
+    // define: {
+    //   global: {},
+    // },
+  };
 });
