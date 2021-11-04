6ce79cc32dedef7ee9f586e10ffa1868bfe80fd1
kazuma takiuchi <basstrombone413@gmail.com>
2021-11-04 22:46:22 +0900

    add initial animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { Box, ChakraProvider, Grid } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  Grid,
+  ScaleFade,
+  Image,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 import { RecoilRoot } from "recoil";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Timeline from "./components/Timeline";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  if (isOpen) {
+    setTimeout(() => {
+      console.log(1);
+      setIsOpen(false);
+    }, 2000);
+  }
+
   return (
     <ChakraProvider>
       <RecoilRoot>
         <Box w="100vw" h="100vh">
+          <ScaleFade initialScale={0.9} in={isOpen} unmountOnExit={true}>
+            <Center h="100vh">
+              <motion.div animate={{ scale: 2 }} transition={{ duration: 0.5 }}>
+                <Center>
+                  <Image src="./icon.png" w="30%" />
+                </Center>
+              </motion.div>
+            </Center>
+          </ScaleFade>
+
           <Grid templateColumns="55px 1fr">
             <Sidebar />
             <Grid templateRows="40px 1fr">
